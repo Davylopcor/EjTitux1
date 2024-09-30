1cf4eeb45e1d38f3be8f956dd0c550b69da19c0d
Gurjot Singh <51828548+GurjotSingh23@users.noreply.github.com>
2024-09-30 12:41:42 +0200

    distribucion

--- PorrasFutboll/src/main/java/com/mycompany/porrasfutboll/JuegoCartas.java
+++ PorrasFutboll/src/main/java/com/mycompany/porrasfutboll/JuegoCartas.java
@@ -5,7 +5,20 @@ package com.mycompany.porrasfutboll;
  * @author David López Corella, Alejandro Rosano, Gurjot Singh y Hector Sanchez
  */
 public class JuegoCartas {
-
+/**
+Hector:
+- Contructores Baraja y Carta
+- cartasMonton
+David:
+- Menú
+- darCartas
+Gurjot:
+- siguienteCarta
+- mostrarBaraja
+Rosano:
+- Barajar
+- cartasDisponibles 
+ */
     public static void main(String[] args) {
         
     }
